cfbb324b8a9c0ec878b2e3070043286e6f60037d
Fau <yannickdu12@gmail.com>
2021-05-09 13:00:15 +0200

    Ajout de la page de style sur les page circuit et circuits

--- AppWeb/Vue/circuit.php
+++ AppWeb/Vue/circuit.php
@@ -25,6 +25,7 @@ if($dataVannes != 0){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="./Style/style.css" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
