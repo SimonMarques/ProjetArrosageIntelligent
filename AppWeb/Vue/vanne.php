d6d5322469a2e6a098845df1d1b4c3cde3fa85d1
Simon <simon.marques@limayrac.fr>
2021-05-18 19:11:54 +0200

    Réglage unitée

--- AppWeb/Vue/vanne.php
+++ AppWeb/Vue/vanne.php
@@ -187,7 +187,7 @@ if($dataProg){
                 yAxes: [{
                     scaleLabel: {
                         display:     true,
-                        labelString: 'ml'
+                        labelString: 'm3/h'
                     }
                 }]
             }
